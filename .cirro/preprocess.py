7285469b3c6b8ec2a6117b1d9429e358831993e4
dimalvovs <dmitrijs.lvovs@gmail.com>
2024-05-17 07:52:15 -0400

    add samplesheet integration with cirro

---
+++ .cirro/preprocess.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+#from https://github.com/break-through-cancer/btc-spatial-pipelines/blob/main/.cirro/preprocess.py
+
+from cirro.helpers.preprocess_dataset import PreprocessDataset
+import pandas as pd
+import numpy as np
+
+SAMPLESHEET_REQUIRED_COLUMNS = ("sample", "data_directory", "npatterns", "niterations")
+
+
+def set_params_as_samplesheet(ds: PreprocessDataset) -> pd.DataFrame:
+    samplesheet = pd.DataFrame([ds.params])
+
+    for colname in samplesheet.columns:
+        if colname not in SAMPLESHEET_REQUIRED_COLUMNS:
+            del samplesheet[colname]
+
+    # Save to a file
+    samplesheet.to_csv("samplesheet.csv", index=None)
+
+    # Clear all nextflow params other than --outdir and --input
+    # since the input samplesheet now contains all the information we need.
+    to_remove = []
+    for k in ds.params:
+        if k != "outdir":
+            to_remove.append(k)
+
+    for k in to_remove:
+        ds.remove_param(k)
+
+    ds.add_param("input", "samplesheet.csv")
+
+    # Log the samplesheet
+    ds.logger.info(samplesheet)
+
+
+def main():
+    ds = PreprocessDataset.from_running()
+
+    set_params_as_samplesheet(ds)
+
+    # log
+    ds.logger.info(ds.params)
+
+
+if __name__ == "__main__":
+    main()
